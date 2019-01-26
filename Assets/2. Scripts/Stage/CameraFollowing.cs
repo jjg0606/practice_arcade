7a21c1b935a94e348b41c8866e71eea7e31d8fb6
jjg0606 <ccm7772003@naver.com>
2019-01-26 09:20:42 +0900

    0126

---
+++ Assets/2. Scripts/Stage/CameraFollowing.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowing : MonoBehaviour
+{
+    private Transform tr;
+    private Transform playerTr;
+    private Vector3 setups;
+
+    public float cameraDampingHorizen = 1.0f;
+    public float cameraDampingVertical = 1.0f;
+    [Header("Camera Outline")]
+    public float limitYdown;
+    public float limitYup;
+    public float limitXleft;
+    public float limitXright;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        playerTr = GameObject.FindGameObjectWithTag("PLAYER").GetComponent<Transform>();
+        setups = new Vector3(0f, 0f, -10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        var currentX = tr.position.x;
+        var wantedX = playerTr.position.x;
+        
+        var currentY = tr.position.y;
+        var wantedY = playerTr.position.y;
+
+        if (wantedX < limitXleft)
+        {
+            wantedX = limitXleft;
+        }
+        else if (wantedX > limitXright)
+        {
+            wantedX = limitXright;
+        }
+
+        if (wantedY < limitYdown)
+        {
+            wantedY = limitYdown;
+        }
+        else if(wantedY>limitYup)
+        {
+            wantedY = limitYup;
+        }
+        currentX = Mathf.Lerp(currentX, wantedX, cameraDampingHorizen * (float)Time.deltaTime);
+        currentY = Mathf.Lerp(currentY, wantedY, cameraDampingVertical * (float)Time.deltaTime);
+        tr.position = new Vector3(currentX,currentY,-10);
+
+
+    }
+}
---
+++ Assets/2. Scripts/UI/HPbar.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    [Header("HPbar foreground")]
+    public Image[] ihp;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        GameObject.FindGameObjectWithTag("PLAYER").GetComponent<PlayerDamaged>().drefHP += ReflectHP;
+        //PlayerDamaged.drefHP += this.ReflectHP;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReflectHP(int Hpleft)
+    {
+        int hpleft = Hpleft;
+        for(int i=0;i<ihp.Length;i++)
+        {
+            if(hpleft>2)
+            {
+                ihp[i].fillAmount = 1.0f;
+                hpleft -= 2;
+            }
+            else 
+            {
+                ihp[i].fillAmount = hpleft*0.5f;
+                hpleft = 0;
+            }
+        }
+    }
+}
